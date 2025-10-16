16fb365f32125d6f466fcec661a2aa28a653039c
breiner <rodriguezbreiner125@gmail.com>
2025-10-16 07:09:37 -0500

    ok

--- app/schemas/lands.py
+++ app/schemas/lands.py
@@ -2,25 +2,26 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class LandBase(BaseModel):
-    nombre: str = Field(min_length=3, max_length=30)
+    nombre: str = Field(..., min_length=3, max_length=30)
     longitud: float
     latitud: float
-    id_usuario: int
     estado: bool
 
 class LandCreate(LandBase):
+    """Modelo usado para crear una finca"""
     pass
 
-class LandUpdate(BaseModel):    
-    nombre: Optional [str] = Field(default=None, min_length=3, max_length=30)
-    longitud: Optional [float] = None
-    latitud: Optional [float] = None
-    id_usuario: Optional [str] = Field(default=None, min_length=1, max_length=2)
-    estado: Optional [bool] = None
+class LandUpdate(BaseModel):
+    """Modelo usado para actualizar una finca"""
+    nombre: Optional[str] = Field(default=None, min_length=3, max_length=30)
+    longitud: Optional[float] = None
+    latitud: Optional[float] = None
+    estado: Optional[bool] = None
 
 class LandEstado(BaseModel):
+    """Modelo usado para cambiar el estado de una finca"""
     estado: Optional[bool] = None
 
 class LandOut(LandBase):
+    """Modelo de salida (lectura)"""
     id_finca: int
-    # nombre_usuario: str
